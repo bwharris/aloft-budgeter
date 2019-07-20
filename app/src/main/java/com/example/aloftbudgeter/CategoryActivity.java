dbee3c78ad9c6cd9073a34b6a5b17da835762051
Benjamin <benjamin.w.harris@gmail.com>
2019-07-19 22:19:50 -0500

    Change Category Activity options based on frequency selection

--- app/src/main/java/com/example/aloftbudgeter/CategoryActivity.java
+++ app/src/main/java/com/example/aloftbudgeter/CategoryActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import java.lang.reflect.Array;
@@ -16,6 +19,16 @@ public class CategoryActivity extends AppCompatActivity {
     Account account = null;
     List<Integer> catDisplayIndexes = null;
     int position = -1;
+    final Integer[] frequencyView = new Integer[] {
+            R.id.category_once_month_selector,
+            R.id.category_month_label,
+            R.id.category_once_day_selector,
+            R.id.category_weeks_every_label,
+            R.id.category_week_selector,
+            R.id.category_weeks_label,
+            R.id.category_monthly_on_label,
+            R.id.category_monthly_day_selector
+        };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +66,18 @@ public class CategoryActivity extends AppCompatActivity {
         categoryDataAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         ((Spinner)findViewById(R.id.category_selector)).setAdapter(categoryDataAdapter);
         ((Spinner)findViewById(R.id.category_selector)).setSelection(position);
+        ((Spinner)findViewById(R.id.category_selector)).setOnItemSelectedListener(
+            new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    findViewById(R.id.category_name).setVisibility(
+                            i == catDisplayIndexes.size() ? View.VISIBLE : View.GONE
+                    );
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) { }
+        });
 
         findViewById(R.id.category_name).setVisibility(
                 position == catDisplayIndexes.size() ? View.VISIBLE : View.GONE
@@ -83,5 +108,29 @@ public class CategoryActivity extends AppCompatActivity {
         ((Spinner)findViewById(R.id.category_once_month_selector)).setAdapter(onceMonthDataAdapter);
 
         ((Spinner)findViewById(R.id.category_once_day_selector)).setAdapter(dayDataAdapter);
+
+
+    }
+
+    public void onRadioButtonClick(View view){
+        for(Integer i : frequencyView){
+            findViewById(i).setVisibility(View.GONE);
+        }
+        switch (view.getId()){
+            case R.id.category_once:
+                findViewById(R.id.category_once_month_selector).setVisibility(View.VISIBLE);
+                findViewById(R.id.category_month_label).setVisibility(View.VISIBLE);
+                findViewById(R.id.category_once_day_selector).setVisibility(View.VISIBLE);
+                break;
+            case R.id.category_weekly:
+                findViewById(R.id.category_weeks_every_label).setVisibility(View.VISIBLE);
+                findViewById(R.id.category_week_selector).setVisibility(View.VISIBLE);
+                findViewById(R.id.category_weeks_label).setVisibility(View.VISIBLE);
+                break;
+            case R.id.category_monthly:
+                findViewById(R.id.category_monthly_on_label).setVisibility(View.VISIBLE);
+                findViewById(R.id.category_monthly_day_selector).setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
